ccb9b4ce72e1f32a590ceb758483c273a0da92da
Or Roob <orroob28@gmail.com>
2021-05-24 18:09:40 +0300

    Update wfq.cpp

--- wfq.cpp
+++ wfq.cpp
@@ -15,7 +15,9 @@ typedef class Package
 
 public:
 	char Sadd[15], Dadd[15];
-	int time, Sport, Dport, len, weight-1;
+	int time, Sport, Dport, len, weight=1, end_t;
+	float sentPck = 0;
+	bool sent = false;
 
 public:
 	Package() = default;
@@ -40,7 +42,10 @@ public:
 
 ostream& operator<<(ostream& os, const Package& p)
 {
-	os << p.time << ' ' << p.Sadd << ' ' << p.Sport << ' ' << p.Dadd << ' ' << p.Dport << ' ' << p.len << ' ' << p.weight;
+	if (p.weight != 1)
+		os << p.time << ' ' << p.Sadd << ' ' << p.Sport << ' ' << p.Dadd << ' ' << p.Dport << ' ' << p.len << ' ' << p.weight;
+	else
+		os << p.time << ' ' << p.Sadd << ' ' << p.Sport << ' ' << p.Dadd << ' ' << p.Dport << ' ' << p.len;
 	return os;
 }
 
@@ -52,6 +57,7 @@ void print_queue(T q) { // NB: pass by value so the print uses a copy
 	}
 	std::cout << '\n';
 }
+
 
 void Split(string buffer, vector <string> *data)
 {
@@ -66,6 +72,7 @@ void Split(string buffer, vector <string> *data)
 	}
 
 }
+
 void Parse(string buffer, pck *p)
 {
 	/*vector <string> data;
@@ -119,6 +126,63 @@ void Parse(string buffer, pck *p)
 	return;
 }
 
+int sendPacket(vector <pck> *pcktList, queue <pck> *pq) {
+	if ((*pcktList).size() == 1) 
+	{
+		int to_return = (*pcktList)[0].time + (*pcktList)[0].len;
+		(*pq).push((*pcktList).front());
+		(*pcktList).pop_back();
+		return to_return;
+	}
+
+	//array holding the sending times of the packets acording to GPS algorithm
+	int *times;
+	times = (int*)malloc((*pcktList).size() * sizeof(int)); 
+	
+	//counter of the packets in the bus
+	int counter=0;
+	int tempEnding;
+
+	for (int i = 0; i < (*pcktList).size()-1; i++) 
+	{
+		if (counter == 0) 
+		{
+			times[i] = (*pcktList)[i].time + (*pcktList)[i].len;
+		}
+		counter += (*pcktList)[i].weight;
+
+		for (int j = 0; j < i; j++)
+		{
+			if (times[j] < (*pcktList)[i].time)
+			{
+				counter = -(*pcktList)[j].weight;
+				continue;
+			}
+			times[j] = (*pcktList)[i].time + (times[j] - (*pcktList)[i].time) / (float)((*pcktList)[j].weight / counter);
+		}
+		if (counter == 0) counter = 1;
+		times[i] = (*pcktList)[i].time + (*pcktList)[i].len  / (float)((*pcktList)[i].weight / counter);
+	}
+	
+	int j = 0;
+
+	for (int i = 0; i < (*pcktList).size(); i++)
+	{
+		if (times[i] < times[j])
+		{
+			j = i;
+		}
+	}
+	int to_return = (*pcktList)[j].time + (*pcktList)[0].len;
+
+	(*pq).push((*pcktList)[j]);
+	(*pcktList).erase((*pcktList).begin() + j);
+	
+	free(times);
+	return to_return;
+}
+
+
 int main()
 {
 	//std::priority_queue<pck> q;
@@ -134,26 +198,54 @@ int main()
 	
 	cout << "Reading from the file" << endl;
 	//f >> line;
-	getline(f, line);
 
 	char a[200] = { 0 }, b[20] = { 0 };
 	strcpy(a, "10 1.1.1.1 2 2.2.2.2 4 10 2");
 	
-	// Using lambda to compare elements.
-	auto cmp = [](pck left, pck right) { return left.Get_weight() > right.Get_weight(); };
-	std::priority_queue<pck, std::vector<pck>, decltype(cmp)> pq(cmp);
+	queue <pck> pq;
 
-	pck p1;
 	//void(*pck_methods[5])(int); //pck_methods is an array that holds pointers to functions
 	//pck_methods[0] = &(p1.Set_time);
 	//pck_methods[1] = &(p1.Set_len);
 
-	Parse(line, &p1);
-	pq.push(p1);	
 	
 	/*for (pck n : data)
 		q3.push(n);
 	print_queue(q3);
 	*/
+	vector <pck> pcktList;
+
+	int time = 0, endTime=0;
+
+	while (getline(f, line)) {
+		pck p1;
+		Parse(line, &p1);
+
+		if (endTime == 0)
+			endTime = p1.time + p1.len;
+		//pq.push(p1);
+		
+		if (p1.time > endTime) {//sending packet
+			
+			endTime = sendPacket(&pcktList, &pq);
+			//continue;
+		}
+
+		pcktList.push_back(p1);
+	}
+
+	while(pcktList.size() != 0) sendPacket(&pcktList, &pq);
+
+	int sTime = pq.front().time;
+
+	while (pq.size() > 0) {
+		if (sTime < pq.front().time)
+			sTime = pq.front().time;
+		cout << sTime << ": " << pq.front() << "\n";
+		
+		sTime += pq.front().len;
+		pq.pop();
+
+	}
 	return 0;
 }
